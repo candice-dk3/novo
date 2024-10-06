184a83c202933ab3a989f5f43ec6ade16c829534
Candice <candicekeet3@gmail.com>
2024-10-06 23:59:58 +0200

    NavBar Done,  50% of Footer done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,130 @@
 <template>
-<section>
-    
-</section>
+<footer>
+    <div class="content">
+        <div class="about-div">
+            <h2>About</h2>
+            <div class="about-content">
+                <a href="/about">Our Story</a>
+                <a href="/about">Sustainability</a>
+                <a href="/about">Careers</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </div>
+        <div class="customer-S">
+            <h2>Customer Service</h2>
+            <div class="customerS-content">
+                <a href="/">Prices & Payments</a>
+                <a href="/">Shipping</a>
+                <a href="/">Return Policy</a>
+                <a href="/">Terms of Services</a>
+                <a href="/">Privacy Policy</a>
+            </div>   
+        </div>
+        <div class="social-M">
+            <h2>Social Media</h2>
+            <div class="socialM-content">
+                <a href="">
+                    <img :src="twitterLogo" alt="Twitter" class="logo"><h4>@NovoFurniture</h4>
+                </a>
+                <a href="">
+                    <img :src="instaLogo" alt="Instagram" class="logo"><h4>@Novo_Furniture</h4>
+                </a>
+                <a href="">
+                    <img :src="fbLogo" alt="Facebook" class="logo"><h4>Novo Modern Furniture</h4>
+                </a>
+            </div>
+        </div>
+        <div class="Logo">
+            <img :src="couchLogo" alt="Novo Logo" class="couchLogo">
+        </div>
+    </div>
+    <div class="copyRight">
+        <h3>@ Novo {{ new Date().getFullYear() }} | All Rights Reserved</h3>
+    </div>
+</footer>
 </template>
 <script>
 export default {
-    
+    name:'Footer',
+    data(){
+        return{
+            couchLogo:'https://github.com/candice-dk3/novo/blob/frontend/src/assets/novo-couch.png?raw=true',
+            twitterLogo:'https://github.com/candice-dk3/novo/blob/frontend/src/assets/twitter.png?raw=true',
+            instaLogo:'https://github.com/candice-dk3/novo/blob/frontend/src/assets/instagram.png?raw=true',
+            fbLogo:'https://github.com/candice-dk3/novo/blob/frontend/src/assets/facebook.png?raw=true'
+        }
+    }
 }
 </script>
 <style scoped>
-    
+footer{
+    background-color: #0C264E;
+    height: auto;
+    padding: 1rem;
+}
+.content{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr); 
+}
+.couchLogo{
+    border-radius: 50%;
+    width: 19rem;
+    border: 5px solid #CBB26A;
+    margin-top: 3.2rem;
+}
+.social-M, .about-div, .customer-S{
+    padding: 3rem;
+    text-align: start;
+    color: white;
+}
+h2{
+    margin-bottom: 2rem;
+    font-weight: 500;
+}
+.socialM-content a, .about-content a, .customerS-content a{
+    display: flex;
+    padding: 0.2rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    text-decoration: none;
+    color: white;
+}
+.logo{
+    width: 2.4rem;
+    padding: 0.2rem;
+}
+h4{
+    margin-top: 1rem;
+    font-weight: 400;
+}
+h3{
+    color: white;
+    font-weight: 400;
+}
+@media screen and (max-width: 390px) {
+footer{
+    height: auto;
+    padding: 1rem;
+}
+    .content{
+    grid-template-columns: repeat(1, 1fr); 
+}   
+}
+@media screen and (max-width:768px) and (min-width: 391px) {
+.couchLogo{
+    width: 14rem;
+    margin-top: 3.2rem;
+}
+h2{
+    margin-bottom: 2rem;
+    font-size: 1rem;
+}
+.social-M, .about-div, .customer-S{
+    padding: 2rem;
+}
+.logo{
+    width: 2rem;
+    padding: 0.1rem;
+}
+}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,46 +1,69 @@
 <template>
-  <nav>
+<nav v-if="showNavbar" >
     <div class="left-side">
         <router-link to="/"><img :src="logo" alt="Novo Logo" class="logo"></router-link>
     </div>
     <div class="middle">
-        <router-link to="/products">SHOP</router-link>
+        <router-link to="/products">Shop</router-link>
     </div>
     <div class="right-side">
-        <router-link to="/cart"><img :src="cartLogo" alt="Cart Logo" class="cartLogo"></router-link>
         <router-link to="/login">Log In</router-link>
         <router-link to="/signUp">Sign Up</router-link>
-        <router-link to="/account"><img :src="accLogo" alt="Account Logo" class="accLogo"></router-link>
+        <router-link to="/cart"><img :src="cartLogo" alt="Cart Logo" class="Logo"></router-link>
+        <router-link to="/account"><img :src="accLogo" alt="Account Logo" class="Logo"></router-link>
     </div>
-  </nav>
+    <div class="hamburger" @click="toggleMenu">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+    <div class="mobile-menu" :class="{ 'active': mobileMenuActive }">
+        <router-link to="/products">SHOP</router-link>
+        <router-link to="/login">LOG IN</router-link>
+        <router-link to="/signUp">SIGN UP</router-link>
+        <router-link to="/cart">CART</router-link>
+        <router-link to="/account">ACCOUNT</router-link>
+    </div>
+</nav>
 </template>
 <script>
 export default {
     name:'Navbar',
     data(){
         return{
-            logo:'https://github.com/candice-dk3/novo/blob/frontend/src/assets/novo-logo.png?raw=true'
+            logo:'https://github.com/candice-dk3/novo/blob/frontend/src/assets/novo-logo.png?raw=true',
+            cartLogo: 'https://github.com/candice-dk3/novo/blob/frontend/src/assets/cart.png?raw=true',
+            accLogo: 'https://github.com/candice-dk3/novo/blob/frontend/src/assets/account.png?raw=true',
+            mobileMenuActive: false,
+            showNavbar: true
         }
+    },
+    methods: {
+    toggleMenu() {
+        this.mobileMenuActive = !this.mobileMenuActive;
+    },
+    hideNavbar() {
+      this.showNavbar = false;
+    }
     }
 }
 </script>
 <style scoped>   
 nav {
-  padding: 0.2rem;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  background-color: #0C264E;
-  height: 5rem;
+    padding: 0.2rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    background-color: #0C264E;
+    height: 5rem;
+    position: relative;
 }
-
 nav a {
-    padding: 1rem;
-    color: black;
+    color: white;
     text-decoration: none;
-    font-weight: 600;
+    font-weight: 500;
     font-size: 1.4rem;
+    margin-top: 1.4rem;
 }
-
 nav a.router-link-exact-active {
     color: #CBB26A;
 }
@@ -54,7 +77,123 @@ nav a.router-link-exact-active {
     grid-template-columns: repeat(4, 1fr);
 }
 .middle{
-    /* padding: 1rem; */
     margin-top: 1.4rem;
+}
+.Logo{
+    width: 2.5rem;
+    margin-top: -0.5rem;
+}
+.hamburger {
+    display: none;
+    cursor: pointer;
+    position: absolute;
+    top: 2rem;
+    right: 1rem;
+}
+.hamburger .line {
+    width: 2rem;
+    height: 0.2rem;
+    background-color: black;
+    margin-bottom: 0.5rem;
+    }
+.mobile-menu {
+    display: none;
+    position: absolute;
+    top: 5rem;
+    right: 0;
+    background-color: #0C264E;
+    padding: 1rem;
+    width: 100%;
+}
+.mobile-menu a {
+    display: block;
+    margin-bottom: 1rem;
+}
+.mobile-menu.active {
+    display: block;
+}
+@media screen and (max-width: 390px) {
+nav {
+    grid-template-columns: 1fr;
+    height: auto;
+}
+.left-side, .middle, .right-side {
+    grid-column: 1;
+    margin-bottom: 1rem;
+}
+.logo {
+    margin-right: 0;
+}
+.right-side {
+    grid-template-columns: 1fr;
+}
+.Logo {
+    margin-top: 0;
+}
+nav a {
+    font-size: 1.2rem;
+}
+.hamburger {
+    display: block;
+}
+.right-side, .middle {
+    display: none;
+}
+}
+@media screen and (max-width: 768px) and (min-width: 391px) {
+  nav {
+    grid-template-columns: 1fr;
+    height: auto;
+  }
+  .left-side, .middle, .right-side {
+    grid-column: 1;
+    margin-bottom: 1rem;
+  }
+  .logo {
+    /* margin-right: 2rem; */
+    text-align: right;
+  }
+  .right-side {
+    grid-template-columns: 1fr;
+  }
+  .Logo {
+    margin-top: 0;
+  }
+  nav a {
+    font-size: 1.2rem;
+  }
+  .hamburger {
+    display: block;
+  }
+  .right-side, .middle {
+    display: none;
+  }
+}
+@media screen and (max-width: 1024px) and (min-width: 769px){
+  .left-side, .middle, .right-side {
+    grid-column: auto;
+    margin-bottom: 0;
+  }
+  .logo {
+    margin-right: 0rem;
+  }
+  .right-side {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  .Logo {
+    margin-top: -0.5rem;
+  }
+  nav a {
+    font-size: 1.4rem;
+  }
+  .hamburger {
+    display: none;
+  }
+  .middle{
+    margin-top: 0rem;
+  }
+  .right-side, .middle {
+    display: grid;
+  }
 }
 </style>
